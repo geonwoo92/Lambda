1fb4914ee732338700e48401add3cd21540b6ed8
geonwoo92 <cnu201102054@gmail.com>
2024-03-14 09:48:34 +0900

    240314

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package com.erich.api;
 import com.erich.api.account.AccountView;
 //import board.BoardView;
 import com.erich.api.article.ArticleView;
+import com.erich.api.enums.Navigation;
 import com.erich.api.user.UserView;
 import com.erich.api.crawler.CrawlerView;
 
@@ -14,32 +15,10 @@ public class Main {
     public static void main(String[] args) throws IOException, SQLException {
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("=== x-Exit " +
-                    "u-User " +
-                    "b-Board " +
-                    "m-Account " +
-                    "c-Crawler " +
-                    "a-Article"+
-                    "===");
-            switch (sc.next()) {
-                case "x":
-                    return;
-                case "u":
-                    UserView.main(sc);
-                    break;
-//                case "2": BoardView.main(); break;
-                case "m":
-                    AccountView.main(sc);
-                    break;
-                case "c":
-                    CrawlerView.main(sc);
-                    break;
-                case "a":
-                    ArticleView.main(sc);
-                    break;
-            }
+        while (Navigation.navimain(sc)) {
+
         }
     }
 
 }
+
